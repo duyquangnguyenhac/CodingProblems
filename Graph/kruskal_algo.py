8ed53a9a648be4385a27fa7e2bd3c88702efa639
James <duyquangnguyenhac@gmail.com>
2023-02-02 12:27:20 -0500

    Kruskal

--- Graph/kruskal_algo.py
+++ Graph/kruskal_algo.py
@@ -1,3 +1,17 @@
 from union_find_algo import UnionFind
 
+def kruskal(edges_list, n_nodes):
+    # Edges_list = [(a, b, w)]
+    union_find = UnionFind(n_nodes)
+    sorted_edges_list = sorted(edges_list, key= lambda x: x[2])
+    spanning_tree_edges = []
+    for a, b, w in sorted_edges_list:
+        if not union_find.same(a, b):
+            spanning_tree_edges.append((a, b, w))
+            union_find.union(a, b)
+            # Terminate early if we already form a spanning tree
+            if union_find.size(a) == n_nodes:
+                return spanning_tree_edges
+    return spanning_tree_edges
+    
 
